6fd7aee474ef84028d15edff6eef6a33f814c06d
thruy <huuhoang.3002@gmail.com>
2025-12-19 19:42:58 +0700

    cicd workflows

--- backend/test.js
+++ backend/test.js
@@ -4,3 +4,4 @@ mongoose
     .connect("mongodb+srv://20225191:20225191@cluster.zn1axnp.mongodb.net/it4409?retryWrites=true&w=majority&appName=Cluster")
     .then(() => console.log("OK"))
     .catch((err) => console.log("ERR:", err));
+// test thu, khong lien quan den server
